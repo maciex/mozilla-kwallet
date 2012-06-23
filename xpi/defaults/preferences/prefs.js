5b8ce49653b73b126ac9d462fbd899c90f6757db
Maciej Sitarz <macieksitarz@wp.pl>
2012-06-23 12:27:43 +0200

    'folder' value defaults to 'MozillaApp.
    This is just an initialization value, it will be changed to
    "Thunderbird" or "Firefox" depending on the app running the extension.

--- xpi/defaults/preferences/prefs.js
+++ xpi/defaults/preferences/prefs.js
@@ -1,5 +1,6 @@
 pref("extensions.firefox-kde-wallet.wallet", "LocalWallet");
-pref("extensions.firefox-kde-wallet.folder", "Firefox");
+// Just an initialization value, it will be changed to "Thunderbird" or "Firefox" depending on the app running the extension.
+pref("extensions.firefox-kde-wallet.folder", "MozillaApp");
 
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.kwallet@guillermo.molina.description", "chrome://firefox-kde-wallet/locale/overlay.properties");
